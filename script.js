b3cfc9de469edf1f6b22f61b624463b460877a0b
Ben <kimaniben14@gmail.com>
2024-04-07 12:18:29 +0100

    redisgn

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function provideFeedback(name, bmi) {
     const bmiAdviceElem = document.getElementById('bmi-advice');
     const riskAdviceElem = document.getElementById('risk-advice');
     const hospitalAdviceElem = document.getElementById('hospital-advice');
-    const mealPlanElem = document.getElementById('meal-plan-content');
+    const mealPlanElem = document.getElementById('meal-plan');
 
     let bmiAdvice = `Dear ${name}, `;
     let riskAdvice = "";
